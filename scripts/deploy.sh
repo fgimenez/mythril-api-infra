6457216bc0c33b8a86c18b7106cef36c8f426162
Federico Gimenez <federico.gimenez@gmail.com>
2018-11-06 16:46:23 +0100

    add IAM users

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,6 +14,8 @@ export KUBECONFIG=$(pwd)/kubeconfig-${PROVIDER}.yaml
 if [ "${PROVIDER}" = aws ]; then
     terraform output config_map_aws_auth | tee configmap-aws-auth.yaml
 
+    cat iam-users.yaml | tee -a configmap-aws-auth.yaml
+
     kubectl apply -f ./configmap-aws-auth.yaml
     kubectl apply -f ./storageclass.yaml
 
